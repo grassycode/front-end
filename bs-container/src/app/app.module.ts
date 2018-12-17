ae10eaba2c2fffe1e2c4afbffc720796f57c2244
grassycode <ranjanfbc@gmail.com>
2018-12-17 18:00:19 -0500

    sidebar | static

--- bs-container/src/app/app.module.ts
+++ bs-container/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BaseComponent } from './base/base.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BaseComponent
+    BaseComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
